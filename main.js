d398a594e4782f801f3b5e8b58de6c6b53664214
Hovhannisyan2008 <erik.h12@tumo.org>
2023-07-20 17:56:41 +0400

    Im erkrord ashxar

--- main.js
+++ main.js
@@ -2,9 +2,17 @@ let seconds = 60;
 let correctAnswer = 0;
 let incorrectAnswer = 0;
 
-
 function getElement(id) {
     return document.getElementById(id);
+}
+
+function getRandomsport() {
+    return sports[Math.floor(Math.random(sports.length - 1) * 10)]
+}
+
+function main() {
+    spor = getRandomsport();
+    getElement("sport").src = spor.sport;
 }
 
 function timer() {
@@ -12,15 +20,13 @@ function timer() {
     getElement("time").innerHTML = seconds;
     getElement("time").style.color = "#80FF00"
     let countdown = setInterval(function () {
+        main();
         seconds--;
         getElement("time").textContent = seconds;
         if (seconds <= 0) {
             clearInterval(countdown);
 
         }
-
-
-
         if (seconds === 10) {
             getElement("time").style.color = "orange";
         }
@@ -30,7 +36,9 @@ function timer() {
             getElement("time").style.color = "#ff0000";
         }
     }, 1000);
+
 }
+
 function check() {
     let input;
     try {
@@ -50,7 +58,7 @@ function check() {
 function finish() {
     clearInterval(checkInterval);
     let percentage = (correctAnswer / (correctAnswer + incorrectAnswer)) * 100;
- getElement("alertaccuracy").innerHTML =` ${percentage}%`;
+    getElement("alertaccuracy").innerHTML = ` ${percentage}%`;
 }
 let checkInterval = setInterval(check, 50);
 
--- object.js
+++ object.js
@@ -2,5 +2,36 @@ const  sports = [
     {
         "sport": "./nkarner/karateee.jpeg",
         "name":  "Կառատե"
+    },{
+        "sport": "./nkarner/judo.jpeg",
+        "name": "Ձյուդո"
+
+    },{
+        "sport": "./nkarner/tenis.jpeg",
+        "name": "թենիս"
+    },{
+        "sport":"./nkarner/voleybol.jpeg",
+        "name": "Ոլեյբոլ"
+    },{
+        "sport": "./nkarner/shaxmat.jpeg",
+        "name": "Շախմատ"
+    },{
+        "sport": "./nkarner/regbi.jpeg",
+        "name": "Ռեգբի"
+    },{
+        "sport": "./nkarner/footbol.jpeg",
+        "name": "Ֆուտբոլ"
+    },{
+        "sport": "./nkarner/box.jpeg",
+        "name": "Բոքս"
+    },{
+        "sport": "./nkarner/canramart.jpeg",
+        "name": "Ծանրամարտ"
+    },{
+        "sport": "./nkarner/@mbshamart.jpeg",
+        "name": "Ըմբշամարտ"
+    },{
+        "sport": "./nkarner/baskebol.jpeg",
+        "name": "Բասկեբո"
     }
 ]
